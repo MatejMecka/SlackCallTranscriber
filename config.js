8fa0fc19dc8823cd932a4855ca1174ee5a8ef1e7
MatejMecka <matej.plavevski+github@gmail.com>
2018-05-22 22:46:59 +0200

    Init

---
+++ config.js
@@ -0,0 +1,14 @@
+const config = {
+	emailField: '#email' ,
+	passwordField: '#password',
+	signinButton: '#signin_btn',
+	inputField:'#msg_input',
+	url: 'https://[SLACK WORKSPACE NAME].slack.com/call/[SLACK CALL ID]/',
+	email: '[EMAIL HERE]',
+	password: '[PASSWORD]',
+	displayHeight: 1080, // Customize based on your own needs
+	displayWidth: 1440, // Customize based on your own needs
+	fileName: 'output.mkv',
+}
+
+module.exports = config
